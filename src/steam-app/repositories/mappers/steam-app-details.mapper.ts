1a782aa48c1a03e6b50cfd424207e9a093505f32
Diego (Azul) Ferreira <metalloger@gmail.com>
2024-07-12 19:12:10 -0300

    ♻️ (steam-app-details.mapper.ts): rename parameter in toEntity method for clarity ✨ (steam-app-details.mapper.ts): use destructuring in toEntity method for better readability

--- src/steam-app/repositories/mappers/steam-app-details.mapper.ts
+++ src/steam-app/repositories/mappers/steam-app-details.mapper.ts
@@ -7,15 +7,15 @@ import {
 } from 'src/steam-app/entities/steam-app.entity';
 
 export abstract class ISteamAppApiMapper implements IMapper<SteamAppEntity> {
-  abstract toEntity(response: AppData): SteamAppEntity;
+  abstract toEntity(appData: AppData): SteamAppEntity;
 }
 
 export class SteamAppApiMapper extends ISteamAppApiMapper {
-  toEntity(data: AppData): SteamAppEntity {
+  toEntity({ steam_appid, name, type }: AppData): SteamAppEntity {
     const props: SteamAppProps = {
-      app_id: data.steam_appid,
-      name: data.name,
-      type: data.type === 'game' ? SteamAppType.GAME : SteamAppType.DLC,
+      app_id: steam_appid,
+      name: name,
+      type: type === 'game' ? SteamAppType.GAME : SteamAppType.DLC,
     };
 
     return new SteamAppEntity(props);
